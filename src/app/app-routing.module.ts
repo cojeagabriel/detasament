412c4cd0b3d2f3d8559edc61fb522a919192642b
cojeagabriel <cojeagabriel@yahoo.com>
2019-11-16 11:10:58 +0200

    first commit ready to deploy

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { CreateCasualtyComponent } from './components/create-casualty/create-casualty.component';
+import { CreateCaseV2Component } from './components/create-case-v2/create-case-v2.component';
+import { CasesV2Component } from './components/cases-v2/cases-v2.component';
+import { InjuryV2Component } from './components/injury-v2/injury-v2.component';
+import { CanDeactivateGuardGuard } from './guards/can-deactivate-guard.guard';
+import { CreateInjuryV2Component } from './components/create-injury-v2/create-injury-v2.component';
+import { TriageComponent } from './components/triage/triage.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InjuriesV2Component } from './components/injuries-v2/injuries-v2.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, data: { state: '0' } },
+  { path: 'injuries',
+    children: [
+      { path: '', component: InjuriesV2Component, data: { state: '1' } },
+      { path: 'create', component: CreateInjuryV2Component, canDeactivate: [CanDeactivateGuardGuard], data: { state: '2' } },
+      { path: ':id',
+        children: [
+          { path: '', component: InjuryV2Component, data: { state: '2' } },
+          { path: 'edit', component: CreateInjuryV2Component, canDeactivate: [CanDeactivateGuardGuard], data: { state: '3' } }
+        ],
+        data: { state: '2' }
+      },
+    ],
+    data: { state: '1' }
+  },
+  { path: 'cases',
+    children: [
+      { path: '', component: CasesV2Component, data: { state: '1' } },
+      { path: 'create', component: CreateCaseV2Component, canDeactivate: [CanDeactivateGuardGuard], data: { state: '2' } }
+    ]
+  },
+  { path: 'create-casualty', component: CreateCasualtyComponent, canDeactivate: [CanDeactivateGuardGuard], data: { state: '1' } },
+  { path: 'triage', component: TriageComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,137 @@
+import { CasualtyService } from './services/casualty.service';
+import { InjuryService } from './services/injury.service';
+import { ManeuverService } from './services/maneuver.service';
+import { CaseService } from 'src/app/services/case.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatSelectModule,
+  MatAutocompleteModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatCardModule,
+  MatListModule,
+  MatTreeModule,
+  MatSlideToggleModule,
+  MatExpansionModule,
+  MatTabsModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatSidenavModule,
+  MatRippleModule,
+  MatMenuModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VictimsListComponent } from './components/victims-list/victims-list.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { CaseComponent } from './components/case/case.component';
+import { CaseCodeComponent } from './components/case-code/case-code.component';
+import { BaremComponent } from './components/barem/barem.component';
+import { InjuriesToStringPipe } from './pipes/injuries-to-string.pipe';
+import { ManeuverComponent } from './components/barem/injury/maneuver/maneuver.component';
+import { CasualtyCardComponent } from './components/casualty-card/casualty-card.component';
+import { TriageComponent } from './components/triage/triage.component';
+import {CaseCardComponent} from './components/case-card/case-card.component';
+import {InjuriesCardComponent} from './components/injuries-card/injuries-card.component';
+import { CasesComponent } from './components/cases/cases.component';
+import { AddManeuverComponent } from './components/add-maneuver/add-maneuver.component';
+import { ManeuverListItemComponent } from './components/maneuver-list-item/maneuver-list-item.component';
+import { DialogAddScoreComponent } from './components/dialog-add-score/dialog-add-score.component';
+import { InjuryListItemComponent } from './components/injury-list-item/injury-list-item.component';
+import { BaremInjuryComponent } from './components/barem-injury/barem-injury.component';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+import { CreateInjuryV2Component } from './components/create-injury-v2/create-injury-v2.component';
+import { InjuriesV2Component } from './components/injuries-v2/injuries-v2.component';
+import { InjuryV2Component } from './components/injury-v2/injury-v2.component';
+import { CreateCaseV2Component } from './components/create-case-v2/create-case-v2.component';
+import { CasesV2Component } from './components/cases-v2/cases-v2.component';
+import { CreateCasualtyComponent } from './components/create-casualty/create-casualty.component';
+import { AddInjuriesComponent } from './components/add-injuries/add-injuries.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    VictimsListComponent,
+    CaseComponent,
+    CaseCodeComponent,
+    BaremComponent,
+    InjuriesToStringPipe,
+    ManeuverComponent,
+    CasualtyCardComponent,
+    TriageComponent,
+    CaseCardComponent,
+    InjuriesCardComponent,
+    CasesComponent,
+    AddManeuverComponent,
+    ManeuverListItemComponent,
+    DialogAddScoreComponent,
+    InjuryListItemComponent,
+    BaremInjuryComponent,
+    ConfirmationDialogComponent,
+    CreateInjuryV2Component,
+    InjuriesV2Component,
+    InjuryV2Component,
+    CreateCaseV2Component,
+    CasesV2Component,
+    CreateCasualtyComponent,
+    AddInjuriesComponent
+  ],
+  entryComponents: [
+    AddManeuverComponent,
+    DialogAddScoreComponent,
+    ConfirmationDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTreeModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+  ],
+  providers: [
+    CaseService,
+    CasualtyService,
+    InjuryService,
+    ManeuverService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
